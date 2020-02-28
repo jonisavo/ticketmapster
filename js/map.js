4d3c031a26a7b6d3706c0234f5eaad6ad7e08671
Topi Volanen <topi.volanen@metropolia.fi>
2020-02-28 12:52:56 +0200

    Toimiva MapsterMarker-luokka ilman extendiä

--- js/map.js
+++ js/map.js
@@ -1,11 +1,40 @@
+console.log("moi");
 let map = L.map('map').setView([60.171972,24.941496], 12);
 
-class MapsterMarker extends L.marker {
-    constructor(temperature) {
-        super();
-        this.temperature = temperature;
+class MapsterMarker{
+    constructor(latlng,options) {
+        this.temperature = 0;
+        this.events = [];
     }
+
+    setTemperature(temp) {
+        this.temperature = temp;
+    }
+    addEvent(event) {
+       this.events.push(event);
+    }
+
+    fetchTemperature() {
+        let osoite = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + 1 + '&lon=' + 1 + '&APPID=3f9c0f263c23c5f5c69f9ace9cc53244';
+        fetch(osoite)
+            .then(function(vastaus){
+                return vastaus.json();
+            })
+            .then(function(json) {
+                setTemperature(json.list[0].main.temp - 273.15);
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+    }
+
 }
+
+let testi = new MapsterMarker([1,1],13);
+testi.addEvent("moi");
+testi.fetchTemperature();
+console.log("moi");
+console.log(testi.temperature);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
--- js/ticketmaster.js
+++ js/ticketmaster.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const popupLeveys = document.querySelector('#map').clientWidth * 0.7;
-const popupKorkeus = document.querySelector('#map').clientHeight * 0.75;
+const popupLeveys = document.querySelector('#map').clientWidth * 0.5;
+const popupKorkeus = document.querySelector('#map').clientHeight * 0.55;
 
 fetch('https://app.ticketmaster.com/discovery/v2/events.json?countryCode=FI&apikey=lFzFD4km6ABGdh9aye7qdAbL5yA1AHkb')
     .then(function(vastaus){
@@ -16,34 +16,55 @@ function palautus(palaute) {
     console.log(palaute);
     let totuus = false;
     let markerit = [];
-    let markeri;
+    let sisalto = [];
+    let longlat = [];
+    let oikea = 0;
     for (let a = 0; a < palaute._embedded.events.length; ++a) {
-        const long = palaute._embedded.events[a]._embedded.venues[0].location.longitude;
-        const lat = palaute._embedded.events[a]._embedded.venues[0].location.latitude;
+        let event = palaute._embedded.events[a]
+        const long = event._embedded.venues[0].location.longitude;
+        const lat = event._embedded.venues[0].location.latitude;
         const kuvasrc =
-            '<div class="tiedot">' +
-            '<img src=' + palaute._embedded.events[a].images[0].url + ' width="100%"><img>' +
-            '<div class="tiedotTiedossa">' +
-            '<h3>' + palaute._embedded.events[a].name + '</h3>' +
-            palaute._embedded.events[a].classifications[0].segment.name + ': ' + palaute._embedded.events[a].classifications[0].genre.name + ', ' + palaute._embedded.events[a].classifications[0].subGenre.name +
-            '<p>' + palaute._embedded.events[a].dates.start.localDate + '</p>' +
-            '<p>' + palaute._embedded.events[a]._embedded.venues[0].address.line1 + '</p>' +
-            '<a href="' + palaute._embedded.events[a].url + '" target="_blank">Hanki liput</a>' +
-            '</div>' +
-            '</div>'
-        for (let b = 0; b < markerit.length; ++b) {
-            if (markerit[b].getLatLng().lng == long && markerit[b].getLatLng().lat == lat) {
-                totuus = true;
-                markeri = markerit[b];
+            '<details class="tiedot"><summary>' + event.name + '</summary>' +
+                '<img src=' + event.images[0].url + ' width="100%"><img>' +
+                '<div class="tiedotTiedossa">' +
+                    '<h3>' + event.name + '</h3>' +
+                    event.classifications[0].segment.name + ': ' + event.classifications[0].genre.name + ', ' + event.classifications[0].subGenre.name +
+                    '<p>' + event.dates.start.localDate + '</p>' +
+                    '<p>' + event._embedded.venues[0].address.line1 + '</p>' +
+                    '<a href="' + event.url + '" target="_blank">Hanki liput</a>' +
+                '</div>' +
+            '</details>'
+        if (longlat.length != 0) {
+            let koordinaatit = {
+                "long": long,
+                "lat": lat
+            };
+            for (let b = 0; b < longlat.length; ++b) {
+                if (longlat[b].lat == koordinaatit.lat && longlat[b].long == koordinaatit.long) {
+                    totuus = true;
+                    oikea = b;
+                }
             }
-        }
-        if (totuus) {
-            markeri._popup.setContent(markeri._popup.getContent() + kuvasrc);
+            if (!totuus) {
+                longlat.push({"long": long, "lat": lat});
+            }
         } else {
-            const marker = L.marker([lat, long], 13).addTo(map);
-            marker.bindPopup(kuvasrc, {minWidth: popupLeveys, maxHeight: popupKorkeus});
-            markerit.push(marker);
+            longlat.push({"long": long, "lat": lat});
+        }
+
+        if (totuus) {
+            sisalto[oikea] += kuvasrc;
+        } else {
+            sisalto.push(kuvasrc);
         }
         totuus = false;
+    }
+    for (let c = 0; c < sisalto.length; ++c) {
+        console.log(longlat[c].lat + ' and ' + longlat[c].long);
+        const marker = new MapsterMarker([longlat[c].lat, longlat[c].long], 13).addTo(map);
+        marker.fetchTemperature();
+        console.log(marker.temperature);
+        marker.bindPopup(`<div id="events">${sisalto[c]}</div><div>Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut </div>`, {minWidth: popupLeveys, maxHeight: popupKorkeus});
+        markerit.push(marker);
     }
 }
